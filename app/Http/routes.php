28373b15a239d58368423f57e6e1df7d41020e14
Sylvia <sylviad1221@gmail.com>
2016-11-03 17:26:39 -0400

    initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/post/{id}/{name}', function($id, $name){
+//    return "This is post number ". $id . " " . $name;
+//});
+//
+//Route::get('admin/posts/example', array('as'=>'admin.home',function(){
+//   $url =  route('admin.home');
+//
+//    return "this url is ". $url;
+//
+//}));
+//
+//Route::group(['middleware' => ['web']], function () {
+//
+//});
+
+//Route::get('/post/{id}', 'PostsController@index');
+
+//Route::resource('posts', 'PostsController');
+
+Route::get('/contact', 'PostsController@contact');
+
+Route::get('/post/{id}/{name}/{pw}', 'PostsController@show_post');
---
+++ resources/views/post.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Post Page {{$id}} {{$name}} {{$pw}}</h1>
+
+@stop
